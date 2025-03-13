158bc02302cba8dcbb3673da19e907c5cb8e38a7
Zharick000 <156972052+Zharick000@users.noreply.github.com>
2025-03-13 17:02:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-const campoDeEntrada = document.getElementById('friendName');
+const campoDeEntrada = document.getElementById('amigo');
 const botonAdicionar = document.getElementById('addButton');
 const botonSortear = document.getElementById('drawButton');
-const listaDeAmigos = document.getElementById('friendsList');
-const divResultado = document.getElementById('result');
+const listaDeAmigos = document.getElementById('listaAmigos');
+const divResultado = document.getElementById('resultado');
+
 let amigos = [];
 
 botonAdicionar.addEventListener('click', function () {
-    const nombreAmigo = campoDeEntrada.value.trim(); 
+    const nombreAmigo = campoDeEntrada.value.trim();
     if (nombreAmigo === "") {
         alert("Por favor, inserte un nombre.");
     } else {
@@ -19,14 +20,12 @@ botonAdicionar.addEventListener('click', function () {
 
 function actualizarListaDeAmigos() {
     listaDeAmigos.innerHTML = "";
-
     for (let i = 0; i < amigos.length; i++) {
         const li = document.createElement('li');
         li.textContent = amigos[i];
         listaDeAmigos.appendChild(li);
     }
 }
-
 botonSortear.addEventListener('click', function () {
     if (amigos.length === 0) {
         alert("No hay amigos en la lista. Por favor, agrega al menos un nombre.");
@@ -36,3 +35,4 @@ botonSortear.addEventListener('click', function () {
         divResultado.innerHTML = `El amigo secreto es: ${amigoSecreto}`;
     }
 });
+
